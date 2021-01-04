eb7977d77d5fbfe47425c4295a71b1a4f77a6e29
Oussama Bouthouri <oussamabo@xtech.guru>
2021-01-04 10:06:39 +0100

    moved assets to the current project folder

---
+++ src/components/footer.js
@@ -0,0 +1,115 @@
+// import { Link } from "gatsby"
+import React from "react"
+
+const Header = () => (
+  <footer className="main-footer">
+    <div className="footer1 bg-body-light">
+      <div className="container">
+        <div className="row mb-12">
+          <div className="footer-item col-sm-12 mt-5 col-lg-4">
+            <h6 className="header">Hubert Magazin</h6>
+            <p>
+              Hubert ist das neue Online-Magazin für Holz-Kultur, das
+              hochwertige und mitreißende Beiträge rund um den Baustoff Holz
+              liefert. Hubert wird herausgegeben von Sorpetaler Fensterbau.
+            </p>
+            <div>
+              <a href="<?php echo get_home_url() . '/hubert/about' ?>">
+                Mehr erfahren
+              </a>
+            </div>
+            <div>
+              <a href="mailto:post@hubert-magazin.de">post@hubert-magazin.de</a>
+            </div>
+          </div>
+          <div className="footer-item col-sm-12 mt-5 col-lg-4">
+            <h6 className="header">Folge uns</h6>
+            <ul className="list-unstyled">
+              <li>
+                <a href="<?php echo stripslashes($fb_url); ?>" target="_blank">
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  href="<?php echo stripslashes($twitter_url); ?>"
+                  target="_blank"
+                >
+                  Twitter
+                </a>
+              </li>
+              <li>
+                <a
+                  href="<?php echo stripslashes($pinterest_url); ?>"
+                  target="_blank"
+                >
+                  Pinterest
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-item col-sm-12 my-5 col-lg-4">
+            <h6 className="header">newsletter</h6>
+            <p>
+              Verpasse keine Neuigkeiten von Hubert. Jetzt zum Newsletter
+              anmelden!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr className="hidden-lg-up" />
+    <div className="footer-bottom bg-body-dark py-5 text-center">
+      <div className="container">
+        <div className="row d-flex justify-content-center">
+          <div className="mx-2">©2017 Hubert</div>
+          <div className="mx-2">|</div>
+          <div className="mx-2">
+            <a href="<?php echo get_home_url() . '/hubert/impressum' ?>">
+              Impressum
+            </a>
+          </div>
+          <div className="hidden-sm-down ml-2 mr-0">|</div>
+          <div className="col-sm-12 pl-0 col-md-3 col-lg-2">
+            Powered by
+            <a href="https://www.sorpetaler.de/" target="_blank">
+              Sorpetaler
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div className="newsletter-popup">
+      <div className="popup-header">
+        <div className="container">
+          <div className="row">
+            <div className="header-text col-11 offset-1">
+              <img src="./images/letter.svg" />
+              <p>Nichts verpassen!</p>
+            </div>
+          </div>
+        </div>
+        <div className="close-icon">
+          <img src="./images/close.svg" />
+        </div>
+        <span className="minimise-icon">
+          <img src="./images/arrow-down.svg" />
+        </span>
+      </div>
+      <div className="container">
+        <div className="popup-content row">
+          <div className="popup-text col-6 offset-1">
+            <p>
+              Du willst regelmäßig die neuesten Hubert-Artikel erhalten? Dann
+              melde dich jetzt für unseren Newsletter an.
+            </p>
+          </div>
+          <div className="popup-form col-4 offset-1">if sidebar</div>
+        </div>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,204 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
+import Footer from "../components/footer"
+import Header from "../components/header"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <>
+    <Helmet>
+      <html lang="en" />
+
+      {/* <script>
+      (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-P6DRKLV");
+    </script> */}
+
+      {/* <link rel="stylesheet" href="main.css" /> */}
+    </Helmet>
+
+    <>
+      <Header />
+      <div className="posts">
+        <article
+          className="main-post"
+          style={{
+            backgroundImage:
+              "url(https://www.sorpetaler.de/wp-content/uploads/2019/02/holzart-bestimmen-e1551088414591.jpg)",
+          }}
+        >
+          <div className="content">
+            <div className="category-text"></div>
+            <div className="h1">
+              <a href="<?php the_permalink(); ?>">Article title 1</a>
+            </div>
+            <p className="post-text">paragraph</p>
+            <a className="read-more" href="<?php the_permalink(); ?>">
+              <img
+                className="hidden-md-up"
+                src="./images/icon_arrow_blue.svg"
+              />
+              <img
+                className="hidden-sm-down"
+                src="./images/icon_arrow_brown.svg"
+              />
+            </a>
+          </div>
+        </article>
+        <div className="page-content">
+          <article className="container">
+            <header className="content padded-content">
+              <h1>
+                <a href="#">title</a>
+              </h1>
+            </header>
+            <div className="content padded-content">
+              <article className="grid-item card">
+                <div>
+                  <div className="image-container">
+                    <img
+                      className="card-img-top img-fluid"
+                      src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
+                    />
+                    <div className="category-text">category text</div>
+                  </div>
+                  <div className="card-block">
+                    <p className="card-title">
+                      <a
+                        className="card-title_link"
+                        href="<?php the_permalink();?>"
+                      >
+                        article title
+                      </a>
+                    </p>
+                    <p className="card-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quae et ut nulla consequuntur error officia!
+                    </p>
+                    <a className="read-more" href="<?php the_permalink();?>">
+                      Mehr
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </article>
+          <article className="container">
+            <header className="content padded-content">
+              <h1>
+                <a href="#">title</a>
+              </h1>
+            </header>
+            <div className="content padded-content">
+              <article className="grid-item card">
+                <div>
+                  <div className="image-container">
+                    <img
+                      className="card-img-top img-fluid"
+                      src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
+                    />
+                    <div className="category-text">category text</div>
+                  </div>
+                  <div className="card-block">
+                    <p className="card-title">
+                      <a
+                        className="card-title_link"
+                        href="<?php the_permalink();?>"
+                      >
+                        article title
+                      </a>
+                    </p>
+                    <p className="card-text">description</p>
+                    <a className="read-more" href="<?php the_permalink();?>">
+                      Mehr
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </article>
+          <article className="container">
+            <header className="content padded-content">
+              <h1>
+                <a href="#">title</a>
+              </h1>
+            </header>
+            <div className="content padded-content">
+              <article className="grid-item card">
+                <div>
+                  <div className="image-container">
+                    <img
+                      className="card-img-top img-fluid"
+                      src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
+                    />
+                    <div className="category-text">category text</div>
+                  </div>
+                  <div className="card-block">
+                    <p className="card-title">
+                      <a
+                        className="card-title_link"
+                        href="<?php the_permalink();?>"
+                      >
+                        article title
+                      </a>
+                    </p>
+                    <p className="card-text">description</p>
+                    <a className="read-more" href="<?php the_permalink();?>">
+                      Mehr
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </article>
+          <article className="container">
+            <header className="content padded-content">
+              <h1>
+                <a href="#">title</a>
+              </h1>
+            </header>
+            <div className="content padded-content">
+              <article className="grid-item card">
+                <div>
+                  <div className="image-container">
+                    <img
+                      className="card-img-top img-fluid"
+                      src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
+                    />
+                    <div className="category-text">category text</div>
+                  </div>
+                  <div className="card-block">
+                    <p className="card-title">
+                      <a
+                        className="card-title_link"
+                        href="<?php the_permalink();?>"
+                      >
+                        article title
+                      </a>
+                    </p>
+                    <p className="card-text">description</p>
+                    <a className="read-more" href="<?php the_permalink();?>">
+                      Mehr
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </article>
+        </div>
+        <button>Load more</button>
+      </div>
+      <Footer />
+    </>
+  </>
 )
 
 export default IndexPage
