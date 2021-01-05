44c95acdf85c098864f595e3f7a6263ce1536173
Hamdi Gatri <hamdigatri92@gmail.com>
2021-01-05 09:53:29 +0100

    Put page content in a separate component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Header from "../components/header"
 
 import icon_arrow_blue from "../images/icon_arrow_blue.svg"
 import icon_arrow_brown from "../images/icon_arrow_brown.svg"
-
+import Posts from "../components/posts"
 const IndexPage = () => (
   <>
     <Helmet>
@@ -25,6 +25,140 @@ const IndexPage = () => (
         crossorigin="anonymous"
       ></script>
 
+      <script
+        type="text/javascript"
+        async=""
+        src="https://assets.pinterest.com/js/pinit_main.js?0.24417730193079423"
+      ></script>
+
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js"
+        integrity="sha512-JRlcvSZAXT8+5SQQAvklXGJuxXTouyq8oIMaYERZQasB8SBDHZaUbeASsJWpk0UUrf89DP3/aefPPrlMR1h1yQ=="
+        crossorigin="anonymous"
+      ></script>
+
+      <link rel="dns-prefetch" href="//assets.pinterest.com" />
+      <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+      <link rel="dns-prefetch" href="//s.w.org" />
+      <link
+        rel="stylesheet"
+        id="apsp-font-opensans-css"
+        href="//fonts.googleapis.com/css?family=Open+Sans&#038;ver=4.8.15"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="apsp-frontend-css-css"
+        href="https://www.sorpetaler.de/wp-content/plugins/accesspress-pinterest/css/frontend.css?ver=3.3.1"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="bwg_fonts-css"
+        href="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/css/bwg-fonts/fonts.css?ver=0.0.1"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="sumoselect-css"
+        href="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/css/sumoselect.min.css?ver=3.0.3"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="mCustomScrollbar-css"
+        href="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/css/jquery.mCustomScrollbar.min.css?ver=1.5.56"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="bwg_googlefonts-css"
+        href="https://fonts.googleapis.com/css?family=Ubuntu&#038;subset=greek,latin,greek-ext,vietnamese,cyrillic-ext,latin-ext,cyrillic"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="bwg_frontend-css"
+        href="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/css/styles.min.css?ver=1.5.56"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="wp-postratings-css"
+        href="https://www.sorpetaler.de/wp-content/plugins/wp-postratings/css/postratings-css.css?ver=1.85"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="sage/css-css"
+        href="https://www.sorpetaler.de/wp-content/themes/hubert/dist/styles/main.css?ver=3.16.1"
+        type="text/css"
+        media="all"
+      />
+      <link
+        rel="stylesheet"
+        id="cmplz-cookie-css"
+        href="https://www.sorpetaler.de/wp-content/plugins/complianz-gdpr/assets/css/cookieconsent.min.css?ver=4.7.2"
+        type="text/css"
+        media="all"
+      />
+      <script
+        type="text/javascript"
+        src="https://www.sorpetaler.de/wp-includes/js/jquery/jquery.js?ver=1.12.4"
+      ></script>
+      <script
+        type="text/javascript"
+        src="https://www.sorpetaler.de/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"
+      ></script>
+      <script
+        type="text/javascript"
+        src="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/js/jquery.sumoselect.min.js?ver=3.0.3"
+      ></script>
+      <script
+        type="text/javascript"
+        src="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/js/jquery.mobile.min.js?ver=1.3.2"
+      ></script>
+      <script
+        type="text/javascript"
+        src="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/js/jquery.mCustomScrollbar.concat.min.js?ver=1.5.56"
+      ></script>
+      <script
+        type="text/javascript"
+        src="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/js/jquery.fullscreen-0.4.1.min.js?ver=0.4.1"
+      ></script>
+
+      <script
+        type="text/javascript"
+        src="https://www.sorpetaler.de/wp-content/plugins/photo-gallery/js/scripts.min.js?ver=1.5.56"
+      ></script>
+
+      <script
+        type="text/javascript"
+        src="https://www.sorpetaler.de/wp-content/plugins/mailjet-for-wordpress/src/widget/js/front-widget.js?ver=4.8.15"
+      ></script>
+      <link
+        rel="https://api.w.org/"
+        href="https://www.sorpetaler.de/wp-json/"
+      />
+      <link
+        rel="EditURI"
+        type="application/rsd+xml"
+        title="RSD"
+        href="https://www.sorpetaler.de/xmlrpc.php?rsd"
+      />
+      <link
+        rel="wlwmanifest"
+        type="application/wlwmanifest+xml"
+        href="https://www.sorpetaler.de/wp-includes/wlwmanifest.xml"
+      />
       {/* <script>
       (function(w, d, s, l, i) {
         w[l] = w[l] || [];
@@ -52,160 +186,27 @@ const IndexPage = () => (
           }}
         >
           <div className="content">
-            <div className="category-text"></div>
+            <div className="category-text">Allgemein</div>
             <div className="h1">
-              <a href="<?php the_permalink(); ?>">Article title 1</a>
+              <a href="https://www.sorpetaler.de/hubert/allgemein/holzart-bestimmen/">
+                HOLZART BESTIMMEN: SO GEHT’S!
+              </a>
             </div>
-            <p className="post-text">paragraph</p>
+            <p className="post-text">
+              Egal ob Schreiner, Restaurator oder ambitionierter
+              Freizeithandwerker: Jeder, der mit dem natürlichen Material Holz
+              arbeitet, sollte wissen, mit welcher Holzart genau er es zu tun
+              hat. Lies hier mehr zur Holzbestimmung.
+            </p>
             <a className="read-more" href="<?php the_permalink(); ?>">
               <img className="hidden-md-up" src={icon_arrow_blue} />
               <img className="hidden-sm-down" src={icon_arrow_brown.svg} />
             </a>
           </div>
         </article>
-        <div className="page-content">
-          <article className="container">
-            <header className="content padded-content">
-              <h1>
-                <a href="#">title</a>
-              </h1>
-            </header>
-            <div className="content padded-content">
-              <article className="grid-item card">
-                <div>
-                  <div className="image-container">
-                    <img
-                      className="card-img-top img-fluid"
-                      src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
-                    />
-                    <div className="category-text">category text</div>
-                  </div>
-                  <div className="card-block">
-                    <p className="card-title">
-                      <a
-                        className="card-title_link"
-                        href="<?php the_permalink();?>"
-                      >
-                        article title
-                      </a>
-                    </p>
-                    <p className="card-text">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Quae et ut nulla consequuntur error officia!
-                    </p>
-                    <a className="read-more" href="<?php the_permalink();?>">
-                      Mehr
-                    </a>
-                  </div>
-                </div>
-              </article>
-            </div>
-          </article>
-          <article className="container">
-            <header className="content padded-content">
-              <h1>
-                <a href="#">title</a>
-              </h1>
-            </header>
-            <div className="content padded-content">
-              <article className="grid-item card">
-                <div>
-                  <div className="image-container">
-                    <img
-                      className="card-img-top img-fluid"
-                      src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
-                    />
-                    <div className="category-text">category text</div>
-                  </div>
-                  <div className="card-block">
-                    <p className="card-title">
-                      <a
-                        className="card-title_link"
-                        href="<?php the_permalink();?>"
-                      >
-                        article title
-                      </a>
-                    </p>
-                    <p className="card-text">description</p>
-                    <a className="read-more" href="<?php the_permalink();?>">
-                      Mehr
-                    </a>
-                  </div>
-                </div>
-              </article>
-            </div>
-          </article>
-          <article className="container">
-            <header className="content padded-content">
-              <h1>
-                <a href="#">title</a>
-              </h1>
-            </header>
-            <div className="content padded-content">
-              <article className="grid-item card">
-                <div>
-                  <div className="image-container">
-                    <img
-                      className="card-img-top img-fluid"
-                      src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
-                    />
-                    <div className="category-text">category text</div>
-                  </div>
-                  <div className="card-block">
-                    <p className="card-title">
-                      <a
-                        className="card-title_link"
-                        href="<?php the_permalink();?>"
-                      >
-                        article title
-                      </a>
-                    </p>
-                    <p className="card-text">description</p>
-                    <a className="read-more" href="<?php the_permalink();?>">
-                      Mehr
-                    </a>
-                  </div>
-                </div>
-              </article>
-            </div>
-          </article>
-          <article className="container">
-            <header className="content padded-content">
-              <h1>
-                <a href="#">title</a>
-              </h1>
-            </header>
-            <div className="content padded-content">
-              <article className="grid-item card">
-                <div>
-                  <div className="image-container">
-                    <img
-                      className="card-img-top img-fluid"
-                      src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
-                    />
-                    <div className="category-text">category text</div>
-                  </div>
-                  <div className="card-block">
-                    <p className="card-title">
-                      <a
-                        className="card-title_link"
-                        href="<?php the_permalink();?>"
-                      >
-                        article title
-                      </a>
-                    </p>
-                    <p className="card-text">description</p>
-                    <a className="read-more" href="<?php the_permalink();?>">
-                      Mehr
-                    </a>
-                  </div>
-                </div>
-              </article>
-            </div>
-          </article>
-        </div>
-        <button>Load more</button>
+        <Posts />
       </div>
+
       <Footer />
     </>
   </>
