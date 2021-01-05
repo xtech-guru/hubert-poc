75fb1e845384be6a192395a62a70ae1508c602dd
Hamdi Gatri <hamdigatri92@gmail.com>
2021-01-05 09:52:28 +0100

    Fix header content and style

--- src/components/header.js
+++ src/components/header.js
@@ -4,27 +4,22 @@ import HLogo from "../images/H_logo.svg"
 import HUBERTLogoDesktop from "../images/HUBERT_logo_desktop.svg"
 
 const Header = () => (
-  <header className="main-header container">
-    <div className="social-icons d-flex justify-content-end">
+  <header class="main-header container">
+    <div class="social-icons d-flex justify-content-end">
       <a
-        className="<?php echo $icon['cls']; ?>"
-        href="<?php echo $icon['url']; ?>"
+        class="hubert_fb_url"
+        href="https://www.facebook.com/hubertmagazin"
         target="_blank"
       ></a>
       <a
-        className="<?php echo $icon['cls']; ?>"
-        href="<?php echo $icon['url']; ?>"
-        target="_blank"
-      ></a>
-      <a
-        className="<?php echo $icon['cls']; ?>"
-        href="<?php echo $icon['url']; ?>"
+        class="hubert_pinterest_url"
+        href="https://www.pinterest.de/sorpetaler/hubert-magazin-f%C3%BCr-holzkultur/"
         target="_blank"
       ></a>
     </div>
-    <nav className="navbar navbar-toggleable-sm navbar-light align-items-md-end">
+    <nav class="navbar navbar-toggleable-sm navbar-light align-items-md-end">
       <button
-        className="navbar-toggler navbar-toggler-right"
+        class="navbar-toggler navbar-toggler-right"
         type="button"
         data-toggle="collapse"
         data-target="#mainNavBar"
@@ -32,7 +27,7 @@ const Header = () => (
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span className="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
       </button>
 
       <a className="navbar-brand" href="https://www.sorpetaler.de/hubert/">
@@ -43,86 +38,139 @@ const Header = () => (
           alt="Hubert logo"
         />
         <div className="hidden-md-down">
-          <h1 className="page-title">text</h1>
+          <h1 class="page-title">Magazin Für Holz-Kultur</h1>
         </div>
       </a>
 
-      <div id="mainNavBar" className="collapse navbar-collapse">
-        <ul className="navbar-nav mt-2 mt-md-0 ml-md-auto">
-          <li className="nav-item dropdown">
+      <div id="mainNavBar" class="collapse navbar-collapse">
+        <ul class="navbar-nav mt-2 mt-md-0 ml-md-auto">
+          <li class="nav-item dropdown">
             <a
               id="navbarCategoriesLink"
-              className="nav-link dropdown-toggle"
+              class="nav-link dropdown-toggle"
               href="#"
               data-toggle="dropdown"
               aria-haspopup="true"
               aria-expanded="false"
             >
-              text
+              Rubriken
             </a>
-            <div
-              className="dropdown-menu"
-              aria-labelledby="navbarCategoriesLink"
-            >
-              <a className="dropdown-item" href="#">
-                category 1
+            <div class="dropdown-menu" aria-labelledby="navbarCategoriesLink">
+              <a
+                class="dropdown-item"
+                href="https://www.sorpetaler.de/hubert/category/allgemein/"
+              >
+                Allgemein
               </a>
-              <a className="dropdown-item" href="#">
-                category 2
+              <a
+                class="dropdown-item"
+                href="https://www.sorpetaler.de/hubert/category/der-werkstoff-holz/"
+              >
+                Der Werkstoff Holz
               </a>
-              <a className="dropdown-item" href="#">
-                category 3
+              <a
+                class="dropdown-item"
+                href="https://www.sorpetaler.de/hubert/category/fensterratgeber/"
+              >
+                Fensterratgeber
+              </a>
+              <a
+                class="dropdown-item"
+                href="https://www.sorpetaler.de/hubert/category/nachhaltig-bauen-und-sanieren/"
+              >
+                Nachhaltig Bauen und Sanieren
+              </a>
+              <a
+                class="dropdown-item"
+                href="https://www.sorpetaler.de/hubert/category/persoenlichkeiten/"
+              >
+                Persönlichkeiten
               </a>
             </div>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              nav item
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.sorpetaler.de/hubert/about">
+              Über uns
             </a>
           </li>
-
-          <li className="nav-item">
+          <li class="nav-item">
             <a
-              className="nav-link no-scroll"
+              class="nav-link no-scroll"
               href="#newsletter"
               data-toggle="collapse"
               aria-expanded="false"
               aria-controls="newsletter"
             >
-              nav item 2
+              Newsletter
             </a>
           </li>
         </ul>
-        <ul className="social-icons navbar-nav mt-2 mt-md-0 hidden-sm-down hidden-lg-up">
-          <li className="nav-item">
+        <ul class="social-icons navbar-nav mt-2 mt-md-0 hidden-sm-down hidden-lg-up">
+          <li class="nav-item">
             <a
-              className="nav-link icon <?php echo $icon['cls']; ?>"
-              href="<?php echo $icon['url']; ?>"
+              class="nav-link icon hubert_fb_url"
+              href="https://www.facebook.com/hubertmagazin"
               target="_blank"
-            >
-              scoial icon
-            </a>
+            ></a>
           </li>
-          <li className="nav-item">
+          <li class="nav-item">
             <a
-              className="nav-link icon <?php echo $icon['cls']; ?>"
-              href="<?php echo $icon['url']; ?>"
+              class="nav-link icon hubert_pinterest_url"
+              href="https://www.pinterest.de/sorpetaler/hubert-magazin-f%C3%BCr-holzkultur/"
               target="_blank"
-            >
-              scoial icon 2
-            </a>
+            ></a>
           </li>
         </ul>
       </div>
     </nav>
-    <div className="menu-panel">
-      <div className="collapse newsletter" id="newsletter">
-        <div className="row align-items-center">
-          <div className="col-md-8 text-md-right">
+    <div class="menu-panel">
+      <div class="collapse newsletter" id="newsletter">
+        <div class="row align-items-center">
+          <div class="col-md-8 text-md-right">
             Verpasse keine Neuigkeiten von Hubert. Jetzt zum Newsletter
             anmelden!
           </div>
-          <div className="col-md-4">side bar newsletter</div>
+          <div class="col-md-4">
+            <div class="mailjet_widget_front_container">
+              <div id="mailjet-widget-title-wrap">
+                <h3>Newsletter</h3>{" "}
+              </div>
+              <form
+                method="post"
+                action=""
+                id="mailjetSubscriptionForm"
+                name="wp_mailjet_subscribe_widget-2"
+              >
+                <div class="mailjet-widget-form-group">
+                  <input
+                    type="email"
+                    name="subscription_email"
+                    id="mailjet_widget_email"
+                    required="required"
+                    placeholder="* ihre@email.com"
+                  />
+                  <input
+                    type="hidden"
+                    name="subscription_locale"
+                    id="mailjet_widget_locale"
+                    value="de_DE"
+                  />
+                  <input
+                    type="hidden"
+                    name="action"
+                    value="send_mailjet_subscription_form"
+                  />
+                </div>
+                <input
+                  type="hidden"
+                  name="widget_id"
+                  value="wp_mailjet_subscribe_widget-2"
+                />
+                <input type="submit" value="Registrieren" />
+              </form>
+              <span class="mailjet_widget_form_message"></span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
