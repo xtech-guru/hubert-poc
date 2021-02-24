805318d948b181ae896411a7b0b75038556ca056
Hamdi Gatri <hamdigatri92@gmail.com>
2021-02-24 22:25:18 +0100

    SP-283 - Add static data

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,39 @@
+const static_data = require(`./src/consts/articles`)
+const path = require(`path`)
 /**
  * Implement Gatsby's Node APIs in this file.
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const data = static_data[0]
+  const { title } = data.article.header
+  const slug = slugify(title)
+  createPage({
+    path: `/articles/${slug}`,
+    component: path.resolve(`./src/templates/ArticleTemplate.js`),
+    context: { data },
+  })
+}
+
+function slugify(string) {
+  const a =
+    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;"
+  const b =
+    "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------"
+  const p = new RegExp(a.split("").join("|"), "g")
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, "") // Trim - from end of text
+}
---
+++ src/consts/articles.js
@@ -0,0 +1,316 @@
+const static_data = [
+  {
+    article: {
+      header: {
+        category_text: "Nachhaltig Bauen und Sanieren",
+        title: "Die schönsten Holzhäuser &#8211; Gewinner stehen fest",
+        introduction:
+          " Die Gewinner des Wettbewerbs Die schönsten Holzhäuser 2020 stehen fest. Hier stellen wir ihre Entwürfe vor und erläutern, warum gerade diese die Jury am meisten überzeugt haben.",
+      },
+      rating: {
+        title: "War dieser Artikel hilfreich?",
+      },
+      author: {
+        name: "Sandra Stein ",
+        description: `Sandra ist hauptberuflich Digital Marketing Managerin und
+        beschäftigt sich seit vielen Jahren mit den neuesten Trends im
+        Internet. Seit 2016 arbeitet sie in der Baubranche. Ihr
+        besonderes Interesse gilt dabei dem Thema nachhaltig Bauen und
+        Leben. Weil sie so gerne spricht, ist sie unsere
+        Hauptinterviewführerin.`,
+      },
+      content: `     <p>
+      Bis zum 30. Juni konnten Studierende, Architekt*innen oder
+      Bauherr*innen ihre Entwürfe für ein besonderes Holzhaus beim
+      Wettbewerb &#8220;Die schönsten Holzhäuser 2020&#8221; von Sorpetaler
+      Fensterbau einreichen. Nach der Sommerpause hat sich die Jury beraten
+      und die drei besten Entwürfe gekürt. Welche das sind und warum gerade
+      diese überzeugt haben, verraten wir hier.
+    </p>
+    <h2>1. Platz: Wohnhaus vis-à-vis von luna productions</h2>
+    <p>
+      Das Wohnhaus <em>via-à-vis</em> im schweizerischen Deitingen ist
+      modern und traditionell zugleich. Das Gebäude, das in zwei
+      Gebäudeteilen mit zwei Etagen jeweils zwei Wohnungen mit 2,5 bzw. 5
+      Zimmern beherbergt, spiegelt mit seiner Kombination aus Holz und Beton
+      die Strömungen zeitgenössischer Architektur wider. Und fügt sich
+      gleichzeitig mit seinem Erscheinungsbild wie selbstverständlich in
+      seine dörfliche Umgebung ein.
+    </p>
+    <blockquote>
+      <p>
+        Wir haben den Anspruch, beim Eingriff in bestehende Situationen
+        nicht nur auf die Bedürfnisse des Einzelnen zu achten, sondern mit
+        unserer Intervention die Situation so zu verbessern, dass eine
+        lebendige Nachbarschaft entstehen kann.
+      </p>
+    </blockquote>
+    <p>
+      Dieser Ansatz ist es auch, der die Architekten angetrieben hat. Nadja
+      und Lukas Frei (beide Anfang 30) von luna productions wünschen sich
+      &#8220;mehr Baukultur im Dorf&#8221;, die neue Bauten nicht losgelöst
+      von der Umgebung, rein funktional oder wirtschaftlich in der Blick
+      nimmt. Im Gegenteil: Das Architektenpaar betrachtet ein Gebäude nicht
+      bloß als die Verwirklichung individueller Ideen, sondern immer als
+      Teil eines Ganzen, einer Gemeinschaft.{" "}
+      <div className="float-md-left unpad-left">
+        <img
+          className="alignnone"
+          src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions-2.png"
+          alt="Das Holzhaus vis-à-vis von luna productions aus der Schweiz mit einem Kirschbaum als tragendem Element"
+          width="400"
+          height="677"
+        />
+      </div>
+      &#8220;Entwerfen wir einen Kindergarten, reden wir mit der Lehrperson,
+      den Eltern und den Kindern; bauen wir im Dorf, studieren wir das Dorf
+      und interessieren uns für die Sicht der Nachbarn ebenso wie für die
+      des Bauherrn. Wir haben den Anspruch, beim Eingriff in bestehende
+      Situationen nicht nur auf die Bedürfnisse des Einzelnen zu achten,
+      sondern mit unserer Intervention die Situation so zu verbessern, dass
+      eine lebendige Nachbarschaft entstehen kann&#8221;, so Nadja und
+      Lukas.
+    </p>
+    <p>
+      Genau das ist ihnen dabei aus Sicht der Jury gelungen. Das neue Haus
+      schließt eine Lücke zwischen der gegenüber liegenden Schreinerei
+      (daher übrigens auch der Name <em>vis-à-vis</em>, französisch für
+      gegenüber) und den umliegenden Wohnbauten. Es entsteht ein gemeinsamer
+      Hof als &#8220;Begegnungsraum für die ganze Nachbarschaft&#8221;, wie
+      Nadja und Lukas es beschreiben. Interessant ist auch der Materialmix,
+      der den klassischen Holzbau weiterentwickelt. Die Außenwände bestehen
+      aus Kalksandstein &#8211; verkleidet mit Holz, der
+      Erschließungsbereich in der Mitte, der beide Gebäudeteile miteinander
+      verbindet, ist aus rohem Beton. Im Inneren wiederum hat luna
+      productions auf eine nichttragende Holzständerkonstruktion gesetzt,
+      die sich wechselnden Bedürfnissen ihrer Bewohner*innen anpassen kann.
+    </p>
+    <p>
+      Auch bei den Themen Wohngesundheit und Regionalität hat das Wohnhaus
+      gepunktet. So haben die Architekten beispielsweise auf heimische
+      Rohstoffe mit kurzen Transportwegen und auf schadstoffarme Anstriche
+      gesetzt. Viele Materialien blieben roh. Die unbehandelte, sägerauhe
+      Fichtenfassade etwa darf natürlich altern. Insgesamt haben die beiden
+      jungen Architekten ein sehr stimmiges Gebäude entwickelt, bei dessen
+      Planung sehr feinfühlig und differenziert unterschiedlichste Aspekte
+      berücksichtigt und miteinander in Einklang gebracht wurden. Ein
+      verdienter erster Platz!
+    </p>
+    <h2>2. Platz: Haus H. von Eva Miclavcic und Eugen Happacher</h2>
+    <p>
+      Haus H. ist ein Einfamilienhaus in Südtirol, das Eva Miclavcic und
+      Eugen Happacher, heute 29 Jahre alt, noch während ihres Studiums an
+      der Akademie der Bildenden Künste München entwerfen durften. Bauherr
+      war ein Ehepaar, das biologische Landwirtschaft betreibt.
+    </p>
+    <blockquote>
+      <p>
+        Ziel war es, ein kleines, einfaches und nachhaltiges Gebäude zu
+        entwickeln, das durch ein zurückhaltendes Äußeres, großzügige Räume
+        und raffinierte Details sich selbstverständlich in die Landschaft
+        einfügt.
+      </p>
+    </blockquote>
+    <p>
+      Dabei standen die Planer vor der Herausforderung, das Wohnhaus in eine
+      traditionelle Umgebung mit bereits bestehenden Wirtschaftsgebäuden
+      einzubetten. &#8220;Ziel war es, ein kleines, einfaches und
+      nachhaltiges Gebäude zu entwickeln, das durch ein zurückhaltendes
+      Äußeres, großzügige Räume und raffinierte Details sich
+      selbstverständlich in die Landschaft einfügt und den Bogen von
+      traditioneller bis hin zu moderner Bauweise spannt&#8221;, beschreiben
+      es Eva und Eugen in ihrer Bewerbung. Was sofort ins Auge sticht:
+      Optisch fällt das Wohnhaus neben den landwirtschaftlichen Gebäuden
+      kaum auf &#8211; bis auf die Fassade vielleicht, die auf den Fotos
+      noch ganz frisch wirkt. Ansonsten aber fügt es sich unglaublich
+      natürlich in seine Umgebung ein.
+      <div className="float-md-left unpad-left">
+        <img
+          className="alignnone"
+          src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-haus-h-eugen-happacher.png"
+          alt="Zweiter Platz beim Wettbewerb Die schönsten Holzhäuser: Haus H in Südtirol von Eva Miclavcic und Eugen Happacher"
+          width="500"
+          height="677"
+        />
+      </div>
+    </p>
+    <p>
+      &#8220;Das Gebäude besticht durch seine Einfachheit und Klarheit. Im
+      gewohnten Blick seiner Umgebung eher schlicht und unauffällig. Dies
+      allerdings nur auf den ersten Blick!&#8221;, drückt es Joachim
+      Kranendonck von der Jury der &#8220;Schönsten Holzhäuser&#8221; aus.
+      Denn auf den zweiten Blick offenbaren sich viele moderne Elemente, die
+      den aktuellen Zeitgeist widerspiegeln. Etwa die großzügigen
+      Holzfensterflächen, die den Bewohnern des Hauses ein unglaubliches
+      Bergpanorama eröffnen. Die Lärchenlattung der Fassade in drei
+      verschiedenen Breiten, die sich selbst bei der Haustür fortsetzt. Oder
+      aber auch die großen, luftigen Räume im Inneren.
+    </p>
+    <p>
+      Der zeitgenössische Ansatz zeigt sich ebenfalls beim Thema
+      Nachhaltigkeit: Architekten und Bauherren setzten konsequent auf
+      Holzsorten, die in der Region vorkommen und ließen sie von ansässigen
+      Firmen verbauen. &#8220;Auf aus fossilen Materialien hergestellte
+      Baustoffe wurde so weit wie möglich verzichtet. Die Dämmung der
+      Bodenplatte erfolgte durch recycelbaren Glasschaumschotter und die der
+      Fassade und des Daches aus Holzfaserdämmplatten&#8221;, heißt es in
+      der Bewerbung weiter. Kombiniert mit einem klugen und klaren Entwurf
+      macht dies das Haus H. zu einem würdigen Zweitplatzierten.
+    </p>
+    <h2>3. Platz: Umweltbildungszentrum Kienbergpark von KolbRipke</h2>
+    <p>
+      Das Umweltbildungszentrum, das im Kienbergpark in Berlin
+      Marzahn-Hellersdorf entstanden ist, macht auf besondere Weise
+      deutlich, welche Wandelbarkeit der Werkstoff Holz in sich birgt. Das
+      Zentrum- der Name verrät es schon &#8211; bietet Kitas und Schulen,
+      Jugendlichen, Familien oder anderen Interessierten vielfältige
+      Veranstaltungen und Angebote rund um die Themen Umwelt und
+      Nachhaltigkeit.{" "}
+      <div className="float-md-left unpad-left">
+        <img
+          className="alignnone"
+          src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-umweltbildungszentrum-kienbergpark-kolbripke.png"
+          alt="Platz 3 des Wettbewerbs Die schönsten Holzhäuser: Das Umweltbildungszentrum Kienbergpark von KolpRipke Architekten in Berlin"
+          width="500"
+          height="677"
+        />
+      </div>
+      Umgeben von Gärten, Wiesen, Wäldern und dem Wuhleteich bildet es einen
+      vorbildlichen Raum für Umweltbildung.
+    </p>
+    <p>
+      Insofern ist es nur stimmig, dass die Planer von KolbRipke
+      Gesellschaft von Architekten mbH aus Berlin für dieses Gebäude auf den
+      natürlichen Werkstoff Holz gesetzt haben. Um eine kurze Bauzeit und
+      eine möglichst große Flexibilität gewährleisten zu können, entschieden
+      sie sich für Holz-Raummodule in Holzrahmenbauweise, die zu einem
+      großen Grad vorgefertigt wurden. Aneinandergereiht ergeben die
+      einzelnen Module mit zwischengeschalteten Licht- und
+      Erschließungsfugen das längliche Gebäude. Ein extensiv begrüntes Dach
+      vervollständigt den nachhaltigen Charakter des Umweltbildungszentrums,
+      sodass es sich trotz der modernen Architektur harmonisch in den
+      umliegenden Park einfügt.
+    </p>
+    <blockquote>
+      <p>
+        Das Objekt besticht auf Anhieb durch seine klare Konstruktion, eine
+        Aneinanderreihung von Raummodulen in Holzrahmenbauweise.
+      </p>
+    </blockquote>
+    <p>
+      Im Inneren des Gebäudes steht Wandelbarkeit im Mittelpunkt. Getrennte
+      Räume entstanden durch den Einsatz von leichten Trennwänden,
+      Ausstellungs- und Seminarbereich können mittels Faltschiebewänden
+      flexibel umgestaltet werden. Denkbar wäre selbst der Einsatz des
+      gesamten Gebäudes an einem anderen Ort. &#8220;Das Gebäude stellt
+      einen Prototypen dar, der zeigen soll, was mit einem Holzmodulbau
+      möglich ist. Die leichte Abbaubarkeit und der einfache Transport
+      eröffnen die Chance, dieses System für vielfältigste andere Nutzungen
+      einzusetzen&#8221;, schließen die Architekten von KolbRipke.{" "}
+      <div className="float-md-left unpad-left">
+        <img
+          className="alignnone"
+          src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-umweltbildungszentrum-kienbergpark-kolbripke-skizze.png"
+          alt="Skizze des Umweltbildungszentrum Kienbergpark von KolbRipke Architekten"
+          width="450"
+          height="677"
+        />
+      </div>
+      Gerade dieses hohe Maß an Flexibilität kombiniert mit sleeker
+      Architektur hat die Jury des Wettbewerbs besonders überzeugt.
+      &#8220;Das Objekt besticht auf Anhieb durch seine klare Konstruktion,
+      eine Aneinanderreihung von Raummodulen in Holzrahmenbauweise. Diese
+      Konstruktion lässt eine offene und flexible Grundrissgestaltung zu.
+      Sowohl der hohe Anteil an Vorfertigung als auch klare und einfache
+      Detaillösungen machen das Gebäude zu einem äußerst wirtschaftlichen
+      Element. Ein schönes Beispiel dafür, was Holzbau leisten kann&#8221;,
+      heißt es von Seiten der Experten. Herzlichen Glückwunsch zum dritten
+      Platz!
+    </p>
+    <h2>Neues E-Book mit den Top 10 Holzhaus-Entwürfen</h2>
+    <p>
+      Die vorgestellten drei Entwürfe erhalten als Gewinner des Wettbewerbs
+      &#8220;Die schönsten Holzhäuser&#8221; Preisgelder in Höhe von 1.500,
+      1.000 und 500 Euro. Darüber hinaus finden sie herausgehobene Erwähnung
+      in der zweiten Auflage des E-Books &#8220;Die schönsten
+      Holzhäuser&#8221;, die Anfang 2021 erscheinen wird. Neben den
+      Gewinner-Einsendungen nimmt das neue E-Book sieben weitere
+      Holzhausentwürfe in den Blick, die die Jury aus unterschiedlichen
+      Gründen für erwähnenswert hielt.
+    </p>
+    <p>
+      Die erste Auflage des E-Books &#8220;Die schönsten Holzhäuser&#8221;
+      kannst du dir{" "}
+      <a href="https://www.sorpetaler.de/wp-content/uploads/2020/02/ebook-die-schoensten-holzhaeuser-2019.pdf">
+        hier
+      </a>{" "}
+      kostenfrei herunterladen.
+    </p>
+    <p>
+      <em>
+        Bildmaterial: luna productions/Mark Drotsky &#8211; Eugen Happacher
+        &#8211; Hanns Joosten &#8211; KolbRipke
+      </em>
+    </p>
+    <div className="text-with_link">
+      <div className="row">
+        <div className="col-md-6 img-fluid">
+          <img
+            className="optional-image img-fluid"
+            src="https://www.sorpetaler.de/wp-content/uploads/2019/10/hausbau-mit-holz-klimaschutz.jpg"
+          />
+        </div>
+        <div className="col-md-6 mt-4 mt-md-0">
+          <div className="text_content">
+            {" "}
+            Der Holzhaus-Bau entwickelt sich zu einem echten Trend. Doch
+            nicht nur kurze Bauzeiten und große Flexibilität machen den
+            Werkstoff Holz zu einem beliebten Material für neue Häuser. In
+            unserem Artikel &#8220;Hausbau mit Holz &#8211; ein echter
+            Klimaretter&#8221; zeigen wir auf, welchen Effekt die
+            Entscheidung für ein Holzhaus auf unsere Umwelt haben kann.
+          </div>
+          <img
+            className="link-icon"
+            src="https://www.sorpetaler.de/wp-content/themes/hubert/assets/images/icon_arrow_blue.svg"
+          />
+          <a
+            href="https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/hausbau-mit-holz/"
+            className="text-link"
+          >
+            <span className="ml-2">Jetzt lesen</span>
+          </a>
+        </div>
+      </div>
+    </div>`,
+      related_posts: {
+        title: `Das könnte dir auch gefallen`,
+        posts: [
+          {
+            title: `Holzhaus-Wettbewerb verlängert`,
+            text: `Der Wettbewerb "Die schönsten Holzhäuser 2020" von Sorpetaler
+        wird bis zum 30. Juni 2020 verlängert. Als Gewinne winken
+        Geldpreise bis 1.500€ und eine Erwähnung im nächsten E-Book.`,
+          },
+          {
+            title: `Der Berliner Eisberg &#8211; ein Mehrfamilienhaus aus Holz`,
+            text: `Auf 100m² Grundfläche ein mehrgeschossiges Wohngebäude mit elf
+          Wohnungen errichten, geht das? Ja, das geht. Gezeigt haben es
+          die Berliner Architekten von rundzwei mit einem innovativen
+          Holz-Hybridbau.`,
+          },
+          {
+            title: `Neues E-Book: Die schönsten Holzhäuser`,
+            text: `Holzhäuser können vieles sein: Urtümlich. Modern. Groß. Klein.
+          Urban. Ländlich. Diese vielen Facetten zeigt das neue E-Book
+          "Die schönsten Holzhäuser 2019". Außerdem: Der große
+          Wettbewerb für Architekten, Studierende oder Bauherren auf der
+          Suche nach den schönsten Holzhäusern 2020.`,
+          },
+        ],
+      },
+    },
+  },
+]
+
+module.exports = static_data
