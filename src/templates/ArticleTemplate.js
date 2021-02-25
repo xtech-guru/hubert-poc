5b3e7d7a68f0117e6481c97828a959cb718766f8
Hamdi Gatri <hamdigatri92@gmail.com>
2021-02-25 07:41:21 +0100

    SP-283 - Remove class names implement styled component

--- src/templates/ArticleTemplate.js
+++ src/templates/ArticleTemplate.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/layout"
+import styled from "styled-components"
 
 const ArticleTemplate = ({ pageContext }) => {
   return (
     <Layout>
-      <div className="article">
+      <PageContainer>
         <ArticleContent
           title={pageContext.data.article.header.title}
           content={pageContext.data.article.content}
@@ -13,13 +14,12 @@ const ArticleTemplate = ({ pageContext }) => {
           author={pageContext.data.article.author}
           rating={pageContext.data.article.rating}
         />
-        <ArticleComments />
         <RecommendedPosts
           title={pageContext.data.article.related_posts.title}
           category={pageContext.data.article.header.category_text}
           posts={pageContext.data.article.related_posts.posts}
         />
-      </div>
+      </PageContainer>
     </Layout>
   )
 }
@@ -33,24 +33,24 @@ const ArticleContent = ({
   rating,
 }) => {
   return (
-    <article className="container post-12053 post type-post status-publish format-standard has-post-thumbnail hentry category-nachhaltig-bauen-und-sanieren">
-      <header className="padded-content">
-        <span className="category-text">
+    <ContentContainer>
+      <header>
+        <CategoryText>
           <a href="https://www.sorpetaler.de/hubert/category/nachhaltig-bauen-und-sanieren/">
             {category}
           </a>
-        </span>
-        <div className="h1">
+        </CategoryText>
+        <ArticleTitle>
           <a
             dangerouslySetInnerHTML={{ __html: title }}
             href="https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/die-schoensten-holzhaeuser-gewinner/"
           ></a>
-        </div>
-        <p className="introduction">{introduction}</p>
+        </ArticleTitle>
+        <Introduction>{introduction}</Introduction>
         <hr />
-        <div className="mx-0 px-0 row d-flex justify-content-between align-items-center">
-          <div className="col-sm-12 col-md-auto pl-0">
-            <p className="author">
+        <Author>
+          <div>
+            <p>
               Von{" "}
               <a
                 href="https://www.sorpetaler.de/hubert/author/sandra/"
@@ -60,8 +60,8 @@ const ArticleContent = ({
               </a>
             </p>
           </div>
-          <hr className="px-0 hidden-md-up col-sm-12" />
-          <div className="share col-sm-12 col-md-auto pr-0 pl-0">
+          <hr />
+          <div>
             <span>Teilen</span>
             <a
               href="http://www.facebook.com/sharer.php?u=https%3A%2F%2Fwww.sorpetaler.de%2Fhubert%2Fnachhaltig-bauen-und-sanieren%2Fdie-schoensten-holzhaeuser-gewinner%2F&p[title]=Die sch%C3%B6nsten Holzh%C3%A4user %E2%80%93 Gewinner stehen fest"
@@ -82,33 +82,23 @@ const ArticleContent = ({
               <img src="https://www.sorpetaler.de/wp-content/themes/hubert/assets/images/icon_pinterest_share.svg" />
             </a>
           </div>
-        </div>
-        <hr className="hidden-sm-down" />
-      </header>
-      <div className="mt-3">
-        <img
-          className="article-image img-fluid img-responsive"
-          src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg"
-        />
-      </div>
-      <div
-        dangerouslySetInnerHTML={{ __html: content }}
-        className="content padded-content"
-      ></div>
-
-      <div className="rating padded-content">
+        </Author>
         <hr />
-        <div className="row">
-          <div className="rating-title col-sm-6">{rating.title}</div>
-          <div className="col-sm-6">
-            <div className="rating-widget">
-              <div className="rating-widget_title hidden-sm-down">RATE</div>
+      </header>
+      <div>
+        <ArticleImage src="https://www.sorpetaler.de/wp-content/uploads/2020/10/schoenste-holzhaeuser-vis-a-vis-luna-productions_fassade-e1604049455169.jpg" />
+      </div>
+      <Content dangerouslySetInnerHTML={{ __html: content }}></Content>
+
+      <RatingContent>
+        <hr />
+        <Row>
+          <RatingTitle>{rating.title}</RatingTitle>
+          <div>
+            <RatingWidget>
+              <RatingWidgetTitle>RATE</RatingWidgetTitle>
               <div>
-                <div
-                  id="post-ratings-12053"
-                  className="post-ratings"
-                  data-nonce="3a4ad085c5"
-                >
+                <PostRating>
                   <img
                     id="rating_12053_1"
                     src="https://www.sorpetaler.de/wp-content/plugins/wp-postratings/images/heart/rating_1_off.gif"
@@ -118,37 +108,24 @@ const ArticleContent = ({
                     onmouseout="ratings_off(0, 0, 0);"
                     onclick="rate_post();"
                     onkeypress="rate_post();"
-                    style={{ cursor: "pointer", border: "0px" }}
                   />
                   (No Ratings Yet)
                   <br />
-                  <span
-                    className="post-ratings-text"
-                    id="ratings_12053_text"
-                  ></span>
-                </div>
-                <div
-                  id="post-ratings-12053-loading"
-                  className="post-ratings-loading"
-                >
-                  <img
-                    src="https://www.sorpetaler.de/wp-content/plugins/wp-postratings/images/loading.gif"
-                    width="16"
-                    height="16"
-                    className="post-ratings-image"
-                  />
+                </PostRating>
+                <PostRatingLoading id="post-ratings-12053-loading">
+                  <img src="https://www.sorpetaler.de/wp-content/plugins/wp-postratings/images/loading.gif" />
                   Loading...
-                </div>{" "}
+                </PostRatingLoading>{" "}
               </div>
-            </div>
+            </RatingWidget>
           </div>
-        </div>
+        </Row>
         <hr />
-      </div>
+      </RatingContent>
 
-      <div className="author-block padded-content">
-        <div className="row d-flex align-items-center">
-          <div className="avatar col-sm-12 col-md-auto">
+      <AuthorBlock>
+        <div>
+          <div>
             <a
               href="https://www.sorpetaler.de/hubert/author/sandra/"
               rel="author"
@@ -157,92 +134,61 @@ const ArticleContent = ({
             </a>
           </div>
 
-          <div className="col-sm-12 col-md">
-            <div className="author-block_name">
+          <div>
+            <AuthorBlockName>
               <a
                 href="https://www.sorpetaler.de/hubert/author/sandra/"
                 rel="author"
               >
                 {author.name}
               </a>
-            </div>
+            </AuthorBlockName>
             <div>
-              <div className="author-block_description col-sm-12">
+              <AuthorBlockDescription>
                 {author.description}
-                <div className="read-more col-sm-12 col-md-auto px-0">
+                <ReadMore>
                   <a
                     href="https://www.sorpetaler.de/hubert/author/sandra/"
                     rel="author"
                   >
                     Mehr
                   </a>
-                </div>
-              </div>
+                </ReadMore>
+              </AuthorBlockDescription>
             </div>
           </div>
         </div>
-      </div>
-    </article>
-  )
-}
-
-const ArticleComments = () => {
-  return (
-    <div className="show-comments">
-      <div className="container">
-        <div className="padded-content">
-          <a
-            className="show-comments_number d-flex justify-content-between"
-            data-toggle="collapse"
-            href="#showComments"
-            aria-expanded="false"
-            aria-controls="showComments"
-          >
-            <div>Keine Kommentare</div>
-            <div className="icon"></div>
-          </a>
-          <div className="collapse" id="showComments">
-            <section id="comments" className="comments"></section>
-          </div>
-        </div>
-      </div>
-    </div>
+      </AuthorBlock>
+    </ContentContainer>
   )
 }
 
 const RecommendedPosts = ({ title, category_text, posts }) => {
   return (
-    <div className="posts">
-      <div className="container">
-        <h2 className="related-posts_title mb-sm-3 mb-md-4">{title}</h2>
-        <div className="grid post-list">
+    <Posts>
+      <PostsContainer>
+        <RelatedPostsTitle>{title}</RelatedPostsTitle>
+        <div>
           {posts.map(post => (
-            <article className="grid-item card">
+            <article>
               <div>
-                <div className="image-container">
-                  <img
-                    className="card-img-top img-fluid"
-                    src="https://www.sorpetaler.de/wp-content/uploads/2020/05/afz-assessment-foerderzentrum-neuwied_waechter-architekten.jpg"
-                  />
-                  <div className="category-text">
+                <div>
+                  <img src="https://www.sorpetaler.de/wp-content/uploads/2020/05/afz-assessment-foerderzentrum-neuwied_waechter-architekten.jpg" />
+                  <div>
                     <a href="https://www.sorpetaler.de/hubert/category/nachhaltig-bauen-und-sanieren/">
                       {category_text}
                     </a>{" "}
                   </div>
                 </div>
-                <div className="card-block">
-                  <p className="card-title">
+                <div>
+                  <p>
                     <a
                       dangerouslySetInnerHTML={{ __html: post.title }}
-                      className="card-title_link"
                       href="https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/holzhaus-wettbewerb-verlaengert/"
                     ></a>
                   </p>
-                  <p className="card-text">{post.text}</p>
-                  <a
-                    className="read-more"
-                    href="https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/holzhaus-wettbewerb-verlaengert/"
-                  >
+                  <p>{post.text}</p>
+                  <a href="https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/holzhaus-wettbewerb-verlaengert/">
                     Mehr
                   </a>
                 </div>
@@ -250,9 +196,139 @@ const RecommendedPosts = ({ title, category_text, posts }) => {
             </article>
           ))}
         </div>
-      </div>
-    </div>
+      </PostsContainer>
+    </Posts>
   )
 }
+const PageContainer = styled.div`
+  margin-top: 2.625rem;
+`
+const ContentContainer = styled.article`
+  color: #756b62;
+  width: 1140px;
+  padding-right: 15px;
+  padding-left: 15px;
+  header {
+    padding-left: 77px;
+    padding-right: 233px;
+  }
+`
+const CategoryText = styled.span`
+  background-color: #f86968;
+  font-weight: 700;
+  font-size: 0.875rem;
+  padding: 5px 17px;
+  color: #fff;
+`
+const ArticleTitle = styled.div`
+  a {
+    font-size: 3rem;
+    color: #4b3e31;
+  }
+`
+const Introduction = styled.p`
+  font-size: 1.125rem;
+  color: #9d958e;
+  margin-bottom: 25px;
+`
 
+const Author = styled.div`
+  display: flex;
+  justify-content: space-between !important;
+  align-items: center;
+`
+const ArticleImage = styled.img`
+  width: 100%;
+  height: auto;
+  max-width: 100%;
+  img {
+    vertical-align: middle;
+  }
+`
+const Content = styled.div`
+  margin: 30px 0;
+  color: #756b62;
+  padding-left: 77px;
+  padding-right: 233px;
+`
+
+const RatingContent = styled.div`
+  text-align: center;
+  color: #c7bcb2;
+  font-size: 0.875rem;
+  padding-left: 77px;
+  padding-right: 233px;
+`
+const Row = styled.div`
+  margin-right: -15px;
+  margin-left: -15px;
+`
+
+const RatingTitle = styled.div`
+  text-align: left;
+`
+const RatingWidget = styled.div`
+  text-align: right;
+`
+
+const RatingWidgetTitle = styled.div`
+  margin-right: 25px;
+`
+const PostRating = styled.div`
+  width: 100%;
+  opacity: 1;
+  img{
+    cursor: "pointer", border: "0px"
+  }
+`
+const PostRatingLoading = styled.div`
+  display: none;
+  height: 16px;
+  text-align: left;
+  img {
+    border: 0;
+    padding: 0;
+    margin: 0;
+    width: 16px;
+    height: 16px;
+  }
+`
+
+const AuthorBlock = styled.div`
+  margin: 30px 0 60px;
+  color: #9d958e;
+  text-transform: uppercase;
+  font-size: 0.875rem;
+  text-align: center;
+  padding-left: 77px;
+  padding-right: 233px;
+`
+const AuthorBlockName = styled.div`
+  text-align: left;
+`
+const AuthorBlockDescription = styled.div`
+  padding: 0;
+  text-align: left;
+`
+
+const ReadMore = styled.div`
+  color: #71b3e7;
+`
+const Posts = styled.div`
+  margin-top: 1.3125rem;
+  padding: 0 14px;
+`
+const PostsContainer = styled.div`
+  width: 1140px;
+  max-width: 100%;
+`
+const RelatedPostsTitle = styled.h2`
+  color: #756b62;
+  font-size: 0.875rem;
+  article {
+    padding-right: 30px;
+    padding-bottom: 61px;
+    width: 33.333%;
+  }
+`
 export default ArticleTemplate
