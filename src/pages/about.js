efe4258881c78b963e1a623bcbefd2e4b066396e
fakhergh <fekhergh93@gmail.com>
2021-02-26 14:09:53 +0100

    SP-285 - Refactor and enhance about and impressum pages content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,144 +1,19 @@
 import React from "react"
-import styled from "styled-components"
 
-import { Container, Layout, Seo } from "../components"
+import { Article, Container, Layout, Seo } from "../components"
 import mockData from "../mock/about.json"
-
-const ArticleHeader = ({ title, description }) => {
-  return (
-    <>
-      <h1>{title}</h1>
-      <p dangerouslySetInnerHTML={{ __html: description }} />
-    </>
-  )
-}
-
-const ParagraphBlock = ({ title, descriptions = [] }) => {
-  return (
-    <>
-      {title && <h2>{title}</h2>}
-      {descriptions.map((description, index) => (
-        <p key={index} dangerouslySetInnerHTML={{ __html: description }} />
-      ))}
-    </>
-  )
-}
-
-const Paragraph = ({ paragraph }) => {
-  return (
-    <>
-      {paragraph.blocks.map((block, index) => (
-        <ParagraphBlock
-          key={index}
-          title={block.title}
-          descriptions={block.descriptions}
-        />
-      ))}
-    </>
-  )
-}
 
 const AboutPage = props => {
   return (
     <Layout>
       <Seo title="About" />
       <Container>
-        <ArticleWrapper>
-          <ArticleContentWrapper>
-            <ArticleHeader
-              title={props.header.title}
-              description={props.header.description}
-            />
-
-            <Paragraph paragraph={props.content.paragraph} />
-          </ArticleContentWrapper>
-        </ArticleWrapper>
+        <Article header={props.header} content={props.content} />
       </Container>
     </Layout>
   )
 }
 
-export default AboutPage
-
 AboutPage.defaultProps = mockData
 
-const ArticleWrapper = styled.article`
-  @media (min-width: 576px) {
-    width: 540px;
-    max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
-
-  @media (min-width: 768px) {
-    width: 720px;
-    max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
-
-  @media (min-width: 992px) {
-    width: 960px;
-    max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
-
-  @media (min-width: 1200px) {
-    width: 1140px;
-    max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
-
-  display: block;
-  position: relative;
-  margin-left: auto;
-  margin-right: auto;
-  box-sizing: inherit;
-`
-
-const ArticleContentWrapper = styled.div`
-  display: block;
-  margin: 0 0 20px;
-  color: #756b62;
-
-  * {
-    box-sizing: inherit;
-  }
-
-  @media (min-width: 992px) {
-    padding-left: 77px;
-    padding-right: 233px;
-  }
-
-  @media (min-width: 768px) {
-    margin-bottom: 30px;
-  }
-
-  img {
-    margin-left: 0;
-    margin-right: 0;
-    max-width: 100%;
-    height: auto;
-    vertical-align: middle;
-    border-style: none;
-
-    *,
-    :after,
-    :before {
-      box-sizing: inherit;
-    }
-
-    @media (min-width: 768px) {
-      margin-bottom: 20px;
-      margin-right: 30px;
-      margin-left: -63px;
-      float: left !important;
-    }
-
-    @media (min-width: 992px) {
-      margin-left: -77px;
-    }
-  }
-`
+export default AboutPage
--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -1,161 +1,19 @@
 import React from "react"
-import styled from "styled-components"
 
-import { Layout, Seo } from "../components"
-import props from "../mock/impressum.json"
+import { Article, Container, Layout, Seo } from "../components"
+import mockData from "../mock/impressum.json"
 
-const CompanyInformation = ({
-  bio,
-  address,
-  phoneNumber,
-  email,
-  authorizedRepresentative,
-}) => {
+const AboutPage = props => {
   return (
-    <>
-      {[bio, address, phoneNumber, email, authorizedRepresentative].map(
-        (value, index) => (
-          <p key={index} dangerouslySetInnerHTML={{ __html: value }} />
-        )
-      )}
-    </>
+    <Layout>
+      <Seo title="Impressum" />
+      <Container>
+        <Article header={props.header} content={props.content} />
+      </Container>
+    </Layout>
   )
 }
 
-const CompanyRegistrationInformation = ({
-  ustidnr,
-  registerCourt,
-  registerNumber,
-}) => {
-  return (
-    <p>
-      {[ustidnr, registerCourt, registerNumber].map(value => (
-        <>
-          <span dangerouslySetInnerHTML={{ __html: value }} />
-          <br />
-        </>
-      ))}
-    </p>
-  )
-}
-
-const Paragraph = ({ title, description }) => {
-  return (
-    <>
-      <p dangerouslySetInnerHTML={{ __html: title }} />
-      <p dangerouslySetInnerHTML={{ __html: description }} />
-    </>
-  )
-}
-
-const AboutPage = props => (
-  <Layout>
-    <Seo title="About" />
-    <StyledContainer>
-      <ArticleWrapper>
-        <ArticleContentWrapper>
-          <h1>
-            <a href="https://www.sorpetaler.de/hubert/impressum/">
-              {props.title}
-            </a>
-          </h1>
-
-          <CompanyInformation
-            bio={props.companyInformation.bio}
-            email={props.companyInformation.email}
-            phoneNumber={props.companyInformation.phoneNumber}
-            address={props.companyInformation.address}
-            authorizedRepresentative={
-              props.companyInformation.authorizedRepresentative
-            }
-          />
-
-          <CompanyRegistrationInformation
-            ustidnr={props.companyRegistrationInformation.ustidnr}
-            registerCourt={props.companyRegistrationInformation.registerCourt}
-            registerNumber={props.companyRegistrationInformation.registerNumber}
-          />
-
-          {props.paragraphs.map((paragraph, index) => (
-            <Paragraph
-              key={index}
-              title={paragraph.title}
-              description={paragraph.description}
-            />
-          ))}
-        </ArticleContentWrapper>
-      </ArticleWrapper>
-    </StyledContainer>
-  </Layout>
-)
-
-AboutPage.defaultProps = props
+AboutPage.defaultProps = mockData
 
 export default AboutPage
-
-const StyledContainer = styled.div`
-  margin: 30px 0 60px;
-
-  @media (min-width: 768px) {
-    margin-top: 60px;
-  }
-
-  * {
-    box-sizing: inherit;
-  }
-`
-
-const ArticleWrapper = styled.article`
-  @media (min-width: 576px) {
-    width: 540px;
-    max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
-
-  @media (min-width: 768px) {
-    width: 720px;
-    max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
-
-  @media (min-width: 992px) {
-    width: 960px;
-    max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
-
-  @media (min-width: 1200px) {
-    width: 1140px;
-    max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-  }
-
-  display: block;
-  position: relative;
-  margin-left: auto;
-  margin-right: auto;
-  box-sizing: inherit;
-`
-
-const ArticleContentWrapper = styled.div`
-  display: block;
-  margin: 0 0 20px;
-  color: #756b62;
-
-  * {
-    box-sizing: inherit;
-  }
-
-  @media (min-width: 992px) {
-    padding-left: 77px;
-    padding-right: 233px;
-  }
-
-  @media (min-width: 768px) {
-    margin-bottom: 30px;
-  }
-`
