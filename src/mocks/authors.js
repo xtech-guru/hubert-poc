3a890a60d2fcabf188408d2a76a4f217f23996c6
Hamdi Gatri <hamdigatri92@gmail.com>
2021-03-01 12:32:08 +0100

    SP-287 - Add authors mocked data

---
+++ src/mocks/authors.js
@@ -0,0 +1,68 @@
+const authors = [
+  {
+    author: {
+      name: "SANDRA",
+      img:
+        "https://secure.gravatar.com/avatar/1bd475830113d79a6d0f79eac7fdcdc4?s=96&r=g",
+      description:
+        "Sandra ist hauptberuflich Digital Marketing Managerin und beschäftigt sich seit vielen Jahren mit den neuesten Trends im Internet. Seit 2016 arbeitet sie in der Baubranche. Ihr besonderes Interesse gilt dabei dem Thema nachhaltig Bauen und Leben. Weil sie so gerne spricht, ist sie unsere Hauptinterviewführerin.",
+      publiched_articles: [
+        {
+          title: "Die schönsten Holzhäuser – Gewinner stehen fest",
+          link:
+            "https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/die-schoensten-holzhaeuser-gewinner/",
+        },
+        {
+          title: "Video: Was macht eigentlich ein Restaurator?",
+          link:
+            "https://www.sorpetaler.de/hubert/der-werkstoff-holz/was-macht-ein-restaurator/",
+        },
+        {
+          title: "Holzhaus-Wettbewerb verlängert",
+          link:
+            "https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/holzhaus-wettbewerb-verlaengert/",
+        },
+        {
+          title: "Waldbaden: Balsam für die Seele",
+          link: "https://www.sorpetaler.de/hubert/allgemein/waldbaden/",
+        },
+        {
+          title: "Der Berliner Eisberg – ein Mehrfamilienhaus aus Holz",
+          link:
+            "https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/eisberg-berlin-mehrfamilienhaus-holz/",
+        },
+        {
+          title: "Neues E-Book: Die schönsten Holzhäuser",
+          link:
+            "https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/e-book-holzhaeuser/",
+        },
+        {
+          title:
+            "Falttür aus Holz für die Terrasse: Funktion und Anwendungsbereiche",
+          link:
+            "https://www.sorpetaler.de/hubert/fensterratgeber/falttuer-aus-holz/",
+        },
+        {
+          title: "Die neuesten Trends bei Holz-Haustüren",
+          link:
+            "https://www.sorpetaler.de/hubert/fensterratgeber/trends-bei-haustueren-holz/",
+        },
+        {
+          title: "U-Wert und Co: Warum ist Holz ein guter Wärmedämmer?",
+          link:
+            "https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/waermedaemmung-holz/",
+        },
+        {
+          title:
+            "Wo sich Nachhaltigkeit und Design treffen: das Korkenzieherhaus",
+          link:
+            "https://www.sorpetaler.de/hubert/nachhaltig-bauen-und-sanieren/nachhaltigkeit-und-design-korkenzieherhaus/",
+        },
+      ],
+      arrow_icon:
+        "https://www.sorpetaler.de/wp-content/themes/hubert/assets/images/icon_arrow_blue.svg",
+    },
+  },
+]
+
+module.exports = authors
