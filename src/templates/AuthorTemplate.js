2f4327c99a95e927758216234d26673172c3bcd2
Hamdi Gatri <hamdigatri92@gmail.com>
2021-03-01 22:16:10 +0100

    SP-287 - Reorder imports

--- src/templates/AuthorTemplate.js
+++ src/templates/AuthorTemplate.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { Layout } from "../components"
 import styled from "styled-components"
 import { Link } from "gatsby"
+
+import { Layout } from "../components"
 
 const AuthorTemplate = ({ pageContext }) => {
   const { author } = pageContext.data
