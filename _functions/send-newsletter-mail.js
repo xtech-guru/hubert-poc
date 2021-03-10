5eb6b7d40d0c489c457529ff922739be640eb47f
fakhergh <fekhergh93@gmail.com>
2021-03-10 10:46:49 +0100

    SP-289 - Add response headers for newsletter lambda function

--- _functions/send-newsletter-mail.js
+++ _functions/send-newsletter-mail.js
@@ -3,6 +3,12 @@ const mailjet = require("node-mailjet").connect(
   process.env.MJ_APIKEY_PRIVATE
 )
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, OPTION",
+}
+
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 function isValidEmail(email) {
@@ -11,17 +17,13 @@ function isValidEmail(email) {
 
 exports.handler = async function (event) {
   if (event.httpMethod !== "POST") {
-    return {
-      statusCode: 404,
-    }
+    return { statusCode: 404 }
   }
 
   const { email } = JSON.parse(event.body || "{}")
 
   if (!isValidEmail(email)) {
-    return {
-      statusCode: 400,
-    }
+    return { statusCode: 400 }
   }
 
   try {
@@ -34,12 +36,11 @@ exports.handler = async function (event) {
     return {
       statusCode: request.response.status,
       body: JSON.stringify(request.body),
+      headers,
     }
   } catch ({ statusCode }) {
     console.log(statusCode)
 
-    return {
-      statusCode,
-    }
+    return { statusCode }
   }
 }
