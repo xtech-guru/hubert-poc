9ab947f7dd08ad9b8231e08944eb503f50124d08
fakhergh <fekhergh93@gmail.com>
2021-03-15 01:39:42 +0100

    SP-338 - Import necessary fonts

---
+++ src/fonts/index.js
@@ -0,0 +1,6 @@
+export { default as GTPressuraBold } from "./GTPressuraBold.ttf"
+export { default as GTPressuraBoldItalic } from "./GTPressuraBoldItalic.ttf"
+export { default as GTPressuraRegular } from "./GTPressuraRegular.ttf"
+export { default as GTPressuraRegularItalic } from "./GTPressuraRegularItalic.ttf"
+export { default as MerriWeatherRegular } from "./MerriweatherRegular.ttf"
+export { default as MerriWeatherItalic } from "./MerriweatherItalic.ttf"
