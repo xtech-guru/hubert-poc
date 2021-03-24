3f5b7f12d444404d6d96d1ee25994f6998abf814
Hamdi Gatri <hamdigatri92@gmail.com>
2021-03-24 23:15:56 +0100

    SP-345 - Fix impressum page responsive design

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -48,33 +48,26 @@ const ContentWrapper = styled.article`
   position: relative;
   margin-left: auto;
   margin-right: auto;
-
+  padding-right: 15px;
+  padding-left: 15px;
   @media (min-width: 576px) {
     width: 540px;
     max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
   }
 
   @media (min-width: 768px) {
     width: 720px;
     max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
   }
 
   @media (min-width: 992px) {
     width: 960px;
     max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
   }
 
   @media (min-width: 1200px) {
     width: 1140px;
     max-width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
   }
 
   & div {
