995c935ca83c31a23577a29554d517c87254a1c3
fakhergh <fekhergh@xtech.guru>
2021-03-25 11:52:27 +0100

    SP-292 - Change disqus shortname

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-disqus`,
       options: {
-        shortname: `sorpetaler`,
+        shortname: `sorpetaler-hubert`,
       },
     },
     `gatsby-plugin-image`,
