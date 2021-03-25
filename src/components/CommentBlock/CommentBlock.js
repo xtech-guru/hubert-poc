9aaeee130575e9fcf84e3945d1dfea689aed496a
fakhergh <fekhergh@xtech.guru>
2021-03-25 11:36:23 +0100

    SP-292 - Use dynamic data for disqusConfig object

--- src/components/CommentBlock/CommentBlock.js
+++ src/components/CommentBlock/CommentBlock.js
@@ -1,15 +1,15 @@
 import { Disqus } from "gatsby-plugin-disqus"
-import React from "react"
-import { useLocation } from "@reach/router"
+import React, { useMemo } from "react"
 
-export function CommentBlock() {
-  const location = useLocation()
-
-  const disqusConfig = {
-    url: `${"https://www.sorpetaler.de/" + location.pathname}`,
-    identifier: 1,
-    title: "post1",
-  }
+export function CommentBlock({ slug, title, url }) {
+  const disqusConfig = useMemo(
+    () => ({
+      title,
+      url,
+      identifier: slug,
+    }),
+    [url, slug, title]
+  )
 
   return <Disqus config={disqusConfig} />
 }
