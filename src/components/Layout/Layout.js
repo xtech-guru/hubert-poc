95fbd9b5c46c40335464a2452b6b0711204646ba
Haykel Ben Jemia <hbj@xtech.guru>
2021-04-16 14:48:20 +0100

    SP-289 Move popup from footer to main layout
    Displayed as fixed item so no need to put it inside
    the footer.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,7 +11,9 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import { Header } from "../Header"
 import { Footer } from "../Footer"
-import { Seo } from "../Seo"
+import { GlobalStyle } from "../GlobalStyle"
+import { SEO } from "../SEO"
+import { NewsletterPopup } from "../NewsletterPopup"
 
 export const Layout = ({ seo, children }) => {
   const data = useStaticQuery(graphql`
@@ -30,6 +32,7 @@ export const Layout = ({ seo, children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <main>{children}</main>
       <Footer />
+      <NewsletterPopup />
     </>
   )
 }
