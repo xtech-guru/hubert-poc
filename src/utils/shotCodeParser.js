30aab9a06e40dcb2a89a1dcecfee2def0bbfbf9e
fakhergh <fekhergh@xtech.guru>
2021-04-20 17:09:11 +0100

    SP-294 - Create short coder parser

---
+++ src/utils/shotCodeParser.js
@@ -0,0 +1,33 @@
+import React from "react"
+import ShortcodeParser from "meta-shortcodes"
+
+const iconArrowBlue = require("../images/icon_arrow_blue.svg")
+
+const parser = new ShortcodeParser()
+
+parser.add("float-left", function (opts, content) {
+  return `<div class="float-left">${content}</div>`
+})
+
+parser.add("highlight", function (opts, content) {
+  return `<div class="highlight"><p>${content}</p></div>`
+})
+
+parser.add("text-with-link", function (opts, content) {
+  return `<div class="text-with-link">
+            <div>
+                <div>
+                    <img src="${opts.image}" />
+                </div>
+                <div>
+                    <div>${content}</div>
+                    <img src="${iconArrowBlue}"/>
+                    <a href={${opts["link-url"]}}>{${opts["link-text"]}}/>
+                </div>
+            </div>
+          </div>`
+})
+
+export function parse(content) {
+  return parser.parse(content)
+}
