d8d0e5d5fa1d939e60460496b275fb6bc8494ecd
Ranim Bahri <ranimba@xtech.guru>
2021-06-16 07:54:01 +0100

    Migrate menu component style to css modules

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,15 +1,18 @@
 import React, { useCallback } from "react"
-import styled from "styled-components"
 import { Link } from "gatsby"
 
+import * as styles from "./Menu.module.scss"
 import data from "../../mocks/menu.json"
 import headerLogoSm from "../../images/H_logo.svg"
 import headerLogoLg from "../../images/HUBERT_logo_desktop.svg"
-import burger_menu_icon from "../../images/burger_menu.svg"
 
 const MenuItem = ({ content, route, type, selected, items = [], onClick }) => {
   return (
-    <MenuItemWrapper selected={selected} type={type}>
+    <li
+      selected={selected}
+      type={type}
+      className={selected && type === "collapse" && styles.menuItemSelected}
+    >
       <Link to={route} onClick={onClick} aria-label="Menu">
         {content}
       </Link>
@@ -22,7 +25,7 @@ const MenuItem = ({ content, route, type, selected, items = [], onClick }) => {
           ))}
         </div>
       )}
-    </MenuItemWrapper>
+    </li>
   )
 }
 
@@ -35,14 +38,14 @@ export const Menu = props => {
   )
 
   const [isCollapsed, setIsCollapsed] = React.useState(false)
-
+  const isVisible = !!menuItemsSelectStatus[props.menuItems[2].content]
   const toggleCollapse = useCallback(() => {
     setIsCollapsed(prevState => !prevState)
   }, [])
 
   return (
     <>
-      <NavBarWrapper>
+      <nav className={styles.navbar}>
         <button
           type="button"
           aria-label="navbar button"
@@ -50,16 +53,18 @@ export const Menu = props => {
         >
           <span />
         </button>
-        <MenuWrapper>
+        <div className={styles.menu}>
           <Link to={props.logo.url} aria-label="Logo">
             <img src={headerLogoSm} alt="Hubert logo" width={40} height={27} />
             <img src={headerLogoLg} alt="Hubert logo" width={150} height={44} />
-            <div className="page-title">
+            <div className={styles.pageTitle}>
               <h1>{props.logo.content}</h1>
             </div>
           </Link>
 
-          <StyledMenuList isCollapsed={isCollapsed}>
+          <div
+            className={isCollapsed ? styles.collpasedMenuList : styles.menuList}
+          >
             <ul>
               {props.menuItems.map(item => (
                 <MenuItem
@@ -78,11 +83,14 @@ export const Menu = props => {
                 />
               ))}
             </ul>
-          </StyledMenuList>
-        </MenuWrapper>
-      </NavBarWrapper>
+          </div>
+        </div>
+      </nav>
 
-      <FormWrapper
+      <div
+        className={` ${styles.form}  ${
+          isVisible ? styles.visibleForm : styles.invisibleForm
+        }`}
         visible={!!menuItemsSelectStatus[props.menuItems[2].content]}
       >
         <div>
@@ -140,297 +148,9 @@ export const Menu = props => {
             </div>
           </div>
         </div>
-      </FormWrapper>
+      </div>
     </>
   )
 }
-
-const MenuWrapper = styled.div`
-  @media (min-width: 768px) {
-    display: flex;
-    flex: 1;
-    justify-content: space-between;
-  }
-`
-const StyledMenuList = styled.div`
-  ${({ isCollapsed }) =>
-    `
-    display : ${isCollapsed ? "block" : "none"} ;
-    @media (min-width: 768px) {
-      display: flex !important;
-      align-items:center
-    }
-  `}
-`
-
-const NavBarWrapper = styled.nav`
-  padding-top: 8px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-
-  font-family: "GT Pressura", -apple-system, system-ui, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-weight: 700;
-  text-transform: uppercase;
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    flex-wrap: nowrap;
-    align-items: flex-end !important;
-    justify-content: space-between;
-  }
-
-  button {
-    outline: none;
-    position: absolute;
-    right: 0;
-    color: #7b756f;
-    align-self: flex-start;
-    padding: 0.25rem 0.75rem;
-    font-size: 1.25rem;
-    line-height: 1;
-    background: transparent;
-    border: 1px solid transparent;
-    @media (min-width: 768px) {
-      display: none;
-    }
-    span {
-      display: inline-block;
-      width: 1.5em;
-      height: 1.5em;
-      vertical-align: middle;
-      content: "";
-      background-image: url(${burger_menu_icon});
-      background-position: center;
-      background-size: auto;
-      background-repeat: no-repeat;
-    }
-  }
-
-  a {
-    color: #7b756f;
-    font-size: 1rem;
-    display: inline-block;
-    padding-top: 0.25rem;
-    padding-bottom: 0.25rem;
-    margin-right: 1rem;
-    line-height: inherit;
-    white-space: nowrap;
-    touch-action: manipulation;
-    text-decoration: none !important;
-    background-color: transparent;
-    cursor: pointer;
-
-    img:first-child {
-      @media (min-width: 768px) {
-        display: none !important;
-        vertical-align: middle;
-        border-style: none;
-      }
-    }
-
-    img:nth-child(2) {
-      margin: 0;
-      vertical-align: middle;
-      border-style: none;
-
-      @media (max-width: 767px) {
-        display: none !important;
-      }
-    }
-
-    div {
-      padding-top: 10px;
-      padding-left: 15px;
-
-      h1 {
-        font-size: 1rem;
-        font-weight: 700;
-        text-transform: uppercase;
-        font-family: GT Pressura, -apple-system, system-ui, BlinkMacSystemFont,
-          Segoe UI, Roboto, Helvetica Neue, Arial, sans-serif;
-        line-height: 1.1;
-        color: #7b756f;
-        margin-bottom: 0.5rem;
-        margin-top: 0;
-      }
-    }
-  }
-
-  & .page-title {
-    display: none;
-
-    @media (min-width: 992px) {
-      display: block;
-    }
-  }
-
-  > div {
-    > div {
-      @media (min-width: 768px) {
-        align-items: flex-end;
-      }
-    }
-  }
-  & ul {
-    display: flex;
-    flex-direction: column;
-    margin-top: 0.5rem !important;
-    margin-bottom: 0;
-    padding-left: 0;
-    list-style: none;
-
-    @media (min-width: 768px) {
-      flex-direction: row;
-      margin-top: 0 !important;
-      margin-left: auto !important;
-    }
-
-    & li {
-      position: relative;
-      display: list-item;
-      text-align: -webkit-match-parent;
-      margin-bottom: 0;
-
-      :first-child {
-        > a::after {
-          display: inline-block;
-          width: 0;
-          height: 0;
-          margin-left: 0.3em;
-          vertical-align: middle;
-          content: "";
-          border-top: 0.3em solid;
-          border-right: 0.3em solid transparent;
-          border-left: 0.3em solid transparent;
-        }
-      }
-
-      & a {
-        color: #7b756f;
-        display: block;
-        padding: 0.5em 0em;
-        touch-action: manipulation;
-        text-decoration: none;
-        background-color: transparent;
-        margin: 0;
-
-        @media (min-width: 768px) {
-          padding-right: 0.5rem;
-          padding-left: 0.5rem;
-        }
-      }
-
-      & div {
-        flex-direction: column;
-        position: absolute;
-        top: 100%;
-        left: 0;
-        z-index: 1000;
-        float: left;
-        min-width: 10rem;
-        padding: 0.5rem 0;
-        margin: 0.125rem 0 0;
-        font-size: 1rem;
-        color: #4b3e31;
-        text-align: left;
-        list-style: none;
-        background-color: #fff;
-        background-clip: padding-box;
-        border: 1px solid rgba(0, 0, 0, 0.15);
-
-        & a {
-          display: block;
-          width: 100%;
-          padding: 3px 1.5rem;
-          clear: both;
-          font-weight: 400;
-          color: #4b3e31;
-          text-align: inherit;
-          white-space: nowrap;
-          background: none;
-          border: 0;
-          touch-action: manipulation;
-          text-decoration: none;
-        }
-      }
-    }
-  }
-`
-
-const MenuItemWrapper = styled.li`
-  ${props =>
-    props.selected && props.type === "collapse"
-      ? `
-  @media (min-width: 768px) {
-    background: #f86968;
-    transition: background-color 1s ease, color 1s ease;
-    a {
-      color: #fff !important;
-    }
-  }
-`
-      : ""}
-`
-
-const FormWrapper = styled.div`
-  overflow: hidden;
-  max-height: ${props => (props.visible ? "350px;" : "0px;")}
-  background: #f86968;
-  color: #fff;
-  transition: max-height .5s ease;
-
-  > div {
-    display: block;
-
-    > div {
-      position: relative;
-      display: flex;
-      flex-wrap: wrap;
-      margin: 0;
-      padding: 50px 46px;
-      align-items: center !important;
-      
-      > div {
-        position: relative;
-        min-height: 1px;
-        padding-right: 15px;
-        padding-left: 15px;
-
-        :first-child {
-          @media (min-width: 768px) {
-            text-align: right !important;
-            flex: 0 0 66.6666666667%;
-            max-width: 66.6666666667%;
-          }
-
-        }
-
-        :nth-child(2) {
-
-          @media (min-width: 768px) {
-            flex: 0 0 33.3333333333%;
-            max-width: 33.3333333333%;
-          }
-
-          > div {
-            margin-bottom: 20px;
-
-            & form {
-              > div {
-                margin-bottom: 10px;
-              }
-            }
-          }
-        }
-        
-      }
-      
-
-    }
-  }
-`
 
 Menu.defaultProps = data
