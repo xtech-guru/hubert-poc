b0885aa9a1e32c67f96f5b74057dab6538f5642b
Ranim Bahri <ranimba@xtech.guru>
2021-06-17 10:49:01 +0100

    Fix default siteMetaData title

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== "production") {
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Sorpetaler Fensterbau`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
