3170fe26f7406a9058194c73a49e250e82f15f0e
Ranim Bahri <ranimba@xtech.guru>
2021-06-23 15:29:21 +0100

    Uninstall styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
